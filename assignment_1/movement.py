38c76341322c2163fd64eb238fc1783b9c355d84
Maarten de Jonge <mdejonge1990@gmail.com>
2012-11-02 18:35:46 +0100

    add main-function

--- assignment_1/movement.py
+++ assignment_1/movement.py
@@ -3,6 +3,9 @@ from pylab import *
 from threading import Thread
 import time
 
+LEFT_WHEEL = nxt.PORT_C
+RIGHT_WHEEL = nxt.PORT_B
+
 class WheelMover(Thread):
     def __init__(self, motor, power, distance, brake):
         Thread.__init__(self)
@@ -29,25 +32,33 @@ def move(brick, power, distance):
     rightMover.join()
 
 
-def rotate(brick, power, distance):
-    left_wheel = nxt.Motor(brick, nxt.PORT_C)
-    right_wheel = nxt.Motor(brick, nxt.PORT_B)
+def rotate(brick, port, power, distance):
+    wheel = nxt.Motor(brick, port)
 
-    leftTurner = WheelMover(left_wheel, power, distance, False)
-    #rightTurner = WheelMover(right_wheel, -power, distance, False)
+    turner = WheelMover(wheel, power, distance, False)
 
-    leftTurner.start()
-    #rightTurner.start()
+    turner.start()
 
-    leftTurner.join()
-    #rightTurner.join()
+    turner.join()
 
 def path(brick, tuples):
     for fun, power, distance in tuples:
         fun(brick, power, distance)
         time.sleep(1)
 
-def main()
+def main():
+    # find a brick
+    brick = nxt.find_one_brick()
 
+    turn_right = lambda brick, power, distance: rotate(brick, LEFT_WHEEL, power,
+            distance)
+    turn_left = lambda brick, power, distance: rotate(brick, RIGHT_WHEEL, power,
+            distance)
+    #lijst = [(move, 100, 200),(turn_left, 100, 120), (move, 100, 200),
+    #        (turn_right, 100, 120)] 
+    lijst = [(move, 100, 200),(turn_left, 100, 180), (move, 100, 200),
+            (turn_left, 100, 180), (move, 100, 200),(turn_left, 100, 180),
+            (move, 100, 200)] 
+    path(brick, lijst)
 if __name__ == "__main__":
-    def main()
+    main()

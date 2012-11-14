05c5a198f33f1fac08b766a5a2e59dc4f86ae05a
Inge <inge.becht91@gmail.com>
2012-11-14 13:04:46 +0100

    added all files

---
+++ assignment_2/code/draw_undistorted_beam.m
@@ -0,0 +1,26 @@
+%DRAW_UNDISTORTED_BEAM Draw the undistorted beam
+%   DRAW_UNDISTORTED_BEAM( rho, theta )
+%
+%   Author Davide Scaramuzza - davide.scaramuzza@ieee.org
+%   ETH Zurich - April, 25, 2007
+function draw_undisdtorted_beam( rho, theta , axislimit, angstep, bw, alpha,count)
+
+[x,y] = pol2cart( theta , rho );
+plot( 0,0,'ro');
+hold on;
+plot( y, -x, '.' ); axis([-axislimit axislimit -axislimit axislimit]); axis square; grid on;
+plot( y(1), -x(1), 'ro' );
+plot( y(4), -x(4), 'bo' );
+%set(gcf, 'Visible', 'off')
+str1(1) = {'angstep:'};
+str1(2) = {angstep};
+str1(3) = {'bw:'};
+str1(4) = {bw};
+str1(5) = {'alpha'};
+str1(6) = {alpha};
+text(0.72,0.7,str1)
+countstr = num2str(count)
+path = strcat('fig', countstr, '.jpg') 
+saveas(gcf,path, 'jpg')
+close all
+hold off;

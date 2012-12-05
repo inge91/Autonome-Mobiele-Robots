8bf2fde7f02821c94647f56053267e586dcc53da
Inge <inge.becht91@gmail.com>
2012-12-05 13:48:36 +0100

    config file with working lines! Hooray

--- assignment_3/pose_estimation/configfile_lines.m
+++ assignment_3/pose_estimation/configfile_lines.m
@@ -3,12 +3,12 @@
 
 % Parameters for line extraction
 
-RMIN = 0.1;              % in [m]
-RMAX = 0.5;              % in [m]
-INLIERTHRESHOLD = 0.02;  % in [m]
-MINSEGPOINTS = 7;        % Minimum number of points for a segment
+RMIN = 0.001;              % in [m]
+RMAX = 0.9;              % in [m]
+INLIERTHRESHOLD = 0.2;  % in [m]
+MINSEGPOINTS = 1;        % Minimum number of points for a segment
 %MINSEGLENGTH = 0.1;      % Minimum actual length for a segment [m]
-MINSEGLENGTH = 0.01;      % Minimum actual length for a segment [m]
+MINSEGLENGTH = 0.001;      % Minimum actual length for a segment [m]
 MINCLUPOINTS = 7;        % Minimum number of points for a cluster
 RHOABSDIFF = 0.3;        % Threshold for splitting clusters (no longer used)
 RHOPEAKLEVEL = 0.12;     % Normalized peak level of rhodiff, used for clustering
@@ -23,5 +23,5 @@ OPENRES = 10;            % [degree]
 % Parameters for the scans
 
 % NUMBER OF SCAN POINTS PER SCAN:
-NSCANPOINTS = 722;
+NSCANPOINTS = 1000;
 

ecaf81dcb5cf4a9bbd906967a4781ebec3bc0054
Maarten de Jonge <mdejonge1990@gmail.com>
2012-12-05 17:10:02 +0100

    add CheckPattern and a function to test the accuracy

--- assignment_3/pose_estimation/find_distance.m
+++ assignment_3/pose_estimation/find_distance.m
@@ -15,11 +15,12 @@ for i = 1:length(s1)
     distances = [distances;  LevenshteinDistance(s1, s2)];
     strings = [strings ; s1];
     % Rotate the string by one character
-    s1 = strcat(s1(2:length(s1)), s1(1));
+    s1 = [s1(2:length(s1)) s1(1)];
 end
 
 % Return the smallest element and the string that belongs to it
 index = find(distances == min(distances));
+index = index(1);
 
 string1 = strings(index, :);
 string2 = s2;

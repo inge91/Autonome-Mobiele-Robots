d7a09f78513bab5c0b97f6a3c3239be92190ecf4
Maarten de Jonge <mdejonge1990@gmail.com>
2012-12-13 22:06:19 +0100

    modify control_panel parameters for experimentation

--- assignment_4/pf_slam_students_v2/control_panel.m
+++ assignment_4/pf_slam_students_v2/control_panel.m
@@ -5,49 +5,76 @@ close all
 %   PARAMETERS
 % -------------------------------
 
-% % odo noises
-sigmaX = 0.003; % m
-sigmaTH = deg2rad(0.02); % rad
-PARAMS.Q= [sigmaX^2 0; 0 sigmaTH^2];
+sigmaXs       =   [0.003   0.05   1      0.003   0.003   0.003  0.003  0.003 0.003   0.003 0.003];
+sigmaTHs      =   [0.02    0.02   0.02   0.1     0.5     0.02   0.02   0.02  0.02    0.02  0.02];
+sigmaRs       =   [0.01    0.01   0.01   0.01    0.01    0.1    0.5    0.01  0.01    0.01  0.01];
+sigmaBs       =   [0.01    0.01   0.01   0.01    0.01    0.01   0.01   0.1   0.5     0.01  0.01];
+particleNs   =   [200     200    200    200     200     200    200    200   200     10    1000];
 
-% % observation noises
-sigmaR= 0.01; % m
-sigmaB= deg2rad(0.01); % rad
-PARAMS.R= [sigmaR^2 0; 0 sigmaB^2];
+filenames     =   {'default.png' 'sigmaX_0.05.png' 'sigmaX_1.png' ...
+                   'sigmaTH_0.1.png' 'sigmaTH_0.5.png'            ...
+                   'sigmaRs_0.1.png' 'sigmaRs_0.5.png'            ...
+                   'sigmaBs_0.1.png' 'sigmaBs_0.5'                ...
+                   'particles_10.png' 'particles_1000.png'};
 
-% NUM PARTICLES
-PARAMS.NPARTICLES= 200; 
 
-% laser filtering
-PARAMS.FEAT_MIN_RANGE = 0.05;
-PARAMS.FEAT_MAX_RANGE = 0.4;
+% make sure the script doesn't fail near the end because of slordigheid
+assert( length(sigmaXs) == length(filenames) );
 
-% corner extraction
-PARAMS.FEAT_MIN_ANGLE = deg2rad(150);
-PARAMS.FEAT_MIN_PT_CHK = 4;
-PARAMS.FEAT_MAX_DIST = 90;
 
-% NN association threshold
-PARAMS.NN_MAX_DIST = 18;
+% loop through each test scenario and save the result
+for i = 1:length(sigmaXs)
+    close all
 
-% Odometry filter
-PARAMS.TOL_JMP = 0.02;
+    % % odo noises
+    sigmaX = sigmaXs(i); % m
+    sigmaTH = deg2rad( sigmaTHs(i) ); % rad
+    PARAMS.Q= [sigmaX^2 0; 0 sigmaTH^2];
 
-% Camera relative position respect to the wheels
-PARAMS.CAMERA_POS_DX = 0.065;
-PARAMS.CAMERA_POS_DY = 0.0;
+    % % observation noises
+    sigmaR= sigmaRs(i); % m
+    sigmaB= deg2rad( sigmaBs(i) ); % rad
+    PARAMS.R= [sigmaR^2 0; 0 sigmaB^2];
 
-% % resampling
-EFECTIVE_PERCENTAGE= 0.75; % minimum number of effective particles before resampling
+    % NUM PARTICLES
+    PARAMS.NPARTICLES= particleNs(i); 
 
-% -------------------------------
-%   DATASET LOAD
-% -------------------------------
+    % laser filtering
+    PARAMS.FEAT_MIN_RANGE = 0.05;
+    PARAMS.FEAT_MAX_RANGE = 0.4;
 
-log = ReadLogFile('log.txt');
+    % corner extraction
+    PARAMS.FEAT_MIN_ANGLE = deg2rad(150);
+    PARAMS.FEAT_MIN_PT_CHK = 4;
+    PARAMS.FEAT_MAX_DIST = 90;
 
-% -------------------------------
-%   PARTICLE FILTER SLAM
-% -------------------------------
+    % NN association threshold
+    PARAMS.NN_MAX_DIST = 18;
 
-fastslam1_sim(log, PARAMS);
+    % Odometry filter
+    PARAMS.TOL_JMP = 0.02;
+
+    % Camera relative position respect to the wheels
+    PARAMS.CAMERA_POS_DX = 0.065;
+    PARAMS.CAMERA_POS_DY = 0.0;
+
+    % % resampling
+    EFECTIVE_PERCENTAGE= 0.75; % minimum number of effective particles before resampling
+
+    % -------------------------------
+    %   DATASET LOAD
+    % -------------------------------
+
+    log = ReadLogFile('log.txt');
+
+    % -------------------------------
+    %   PARTICLE FILTER SLAM
+    % -------------------------------
+
+    fastslam1_sim(log, PARAMS);
+
+    % save the image
+    print('-dpng', filenames{i})
+
+    close all
+end
